a6836c53724e30a2155a26d5756d74951054059e
SERGIO URQUIZA <2220201015@estudiantesunibague.edu.co>
2024-05-29 20:53:29 -0500

    Database and ports created and working

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
+require('dotenv').config(); 
+const { sequelize } = require("./database/database.js");
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
-    res.send({ express: 'Hello From Express' });
+    res.send({ express: 'Bienvenido a la API de Dev|Mind, exitos te deseamos Juan y Sergio' });
 }, (error) => {
     console.log(error);
 });
 
-app.listen(port, () => console.log(`Escuchando en el puerto ${port}`));
+async function connectionDataBase() {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync({ force: false});
+        console.log("Conexion exitosa a la base de datos");
+    } catch (error) {
+        console.log("Error en la conexion",error);
+    }
+}
 
-// ENDPOINTS DE LA API 5000
+app.listen(port, () => {
+    console.log(`Activado escuchando en el puerto: ${port}`)
+    connectionDataBase()
+})
