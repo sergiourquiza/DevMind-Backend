e4a7fc9c7ac5e08fa7b6083eece0cb6805a03e6a
SERGIO URQUIZA <2220201015@estudiantesunibague.edu.co>
2024-06-03 15:32:20 -0500

    Some problems have been fixed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 // Importar todas las rutas
 const difficultyRoutes = require('./difficultyRoutes');
+const userRoutes = require('./userRoutes');
+const theoryRoutes = require('./theoryRoutes');
 
 // Usar las rutas
 router.use('/difficulties', difficultyRoutes);
+router.use('/users', userRoutes);
+router.use('/theory', theoryRoutes);
 module.exports = router;
