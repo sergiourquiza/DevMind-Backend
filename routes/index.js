057d88d513322ced99dcf128253e116133b933f5
SERGIO URQUIZA <2220201015@estudiantesunibague.edu.co>
2024-06-08 18:51:49 -0500

    Backend finished

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,8 @@ const progressRoutes = require('./progressRoutes');
 const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const theoryRoutes = require('./theoryRoutes');
-<<<<<<< HEAD
 const excerciseRoutes = require('./exerciseRoutes'); 
-=======
 const userExerciseRoutes = require('./userExerciseRoutes');
->>>>>>> 6c7eabc79791c1920d5378be8138d7bce6e5311f
 
 // Usar las rutas
 router.use('/difficulties', difficultyRoutes);
@@ -22,12 +19,8 @@ router.use('/modules', moduleRoutes);
 router.use('/progresses', progressRoutes);
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
-<<<<<<< HEAD
 router.use('/theories', theoryRoutes);
 router.use('/exercises', excerciseRoutes);
-=======
-router.use('/theory', theoryRoutes);
-router.use('/userExercises', theoryRoutes);
+router.use('/userExercises', userExerciseRoutes);
 
->>>>>>> 6c7eabc79791c1920d5378be8138d7bce6e5311f
 module.exports = router;
