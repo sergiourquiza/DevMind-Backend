e8d6fce5a9907e368d762c3eafeae5f363570913
juancai0104 <2220201024@estudiantesunibague.edu.co>
2024-06-09 11:00:29 -0500

    Fix env config

--- models/index.js
+++ models/index.js
@@ -5,6 +5,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+
 /**
  * Loads all Sequelize models present in the current directory and associates them.
  * @module Index
