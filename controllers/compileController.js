7381e59790c4ec6a4d2974d9e606015cfd1ba43b
Juan Esteban Caicedo Pardo <2220201024@estudiantesunibague.edu.co>
2024-10-14 11:35:08 -0500

    Fix compiler controller to accept different exercises

--- controllers/compileController.js
+++ controllers/compileController.js
@@ -4,15 +4,15 @@ const compilerService = require('../services/compilerService');
 exports.compileCode = async (req, res) => {
     const { exerciseId, language, code } = req.body;
 
+    console.log(language);
+
     try {
-        // Obtener el ejercicio desde la base de datos
         const exercise = await Exercise.findByPk(exerciseId);
 
         if (!exercise) {
             return res.status(404).json({ error: 'Exercise not found' });
         }
 
-        // Obtener todos los inputs y respuestas esperadas desde la base de datos
         const inputs = exercise.requiresInput ? await Input.findAll({
             where: { exerciseId: exerciseId }
         }) : [];
@@ -25,15 +25,12 @@ exports.compileCode = async (req, res) => {
             return res.status(404).json({ error: 'Expected answers not found for the exercise' });
         }
 
-        // Definir el tipo de código y el nombre de la función (si es una función)
-        const codeType = exercise.codeType; // Suponiendo que está almacenado en la base de datos
-        const functionName = exercise.functionName || ''; // Si no es una función, será una cadena vacía
+        const codeType = exercise.codeType;
+        const functionName = exercise.functionName || '';
 
         let results = [];
 
-        // Verificar si el ejercicio requiere inputs
         if (exercise.requiresInput) {
-            // Iterar sobre los inputs y realizar la comparación con las respuestas
             for (let i = 0; i < inputs.length; i++) {
                 const input = inputs[i];
                 const expectedAnswer = answers.find(answer => answer.inputId === input.id);
@@ -41,11 +38,9 @@ exports.compileCode = async (req, res) => {
                 if (expectedAnswer) {
                     const inputsData = input.input;
                     try {
-                        // Enviar el código proporcionado por el usuario, los inputs, codeType y functionName al servicio de compilación
                         const output = await compilerService.compileCode(language, code, inputsData, codeType, functionName);
                         const outputLines = output.trim().split('\n');
 
-                        // Comparar la salida con la respuesta esperada
                         const isCorrect = outputLines.includes(expectedAnswer.answer.trim());
                         const outputLine = outputLines.find(line => line.trim() === expectedAnswer.answer.trim()) || "Not found in output";
 
@@ -62,7 +57,6 @@ exports.compileCode = async (req, res) => {
                 }
             }
         } else {
-            // Si no se requieren inputs, simplemente ejecutamos el código y comparamos la salida
             const output = await compilerService.compileCode(language, code, '', codeType, functionName);
             const outputLines = output.trim().split('\n');
 
@@ -71,7 +65,7 @@ exports.compileCode = async (req, res) => {
                 const outputLine = outputLines.find(line => line.trim() === answer.answer.trim()) || "Not found in output";
 
                 return {
-                    inputId: null,  // No hay input asociado
+                    inputId: null,
                     expectedResult: answer.answer.trim(),
                     isCorrect: isCorrect,
                     output: outputLine,
@@ -80,7 +74,6 @@ exports.compileCode = async (req, res) => {
             });
         }
 
-        // Enviar los resultados
         res.json({ results });
     } catch (err) {
         res.status(500).json({ error: 'Internal server error', details: err.message });
